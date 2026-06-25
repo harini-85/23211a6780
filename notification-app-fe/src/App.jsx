42d1e45b6debad7aa272dc5ebfd59b60a47a5330
Harini Nethi <harininethi8@gmail.com>
2026-06-25 15:23:59 +0530

    added logs

--- notification-app-fe/src/App.jsx
+++ notification-app-fe/src/App.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
 import { NotificationsPage } from "./pages/NotificationsPage";
+import { Log } from "./utils/logger";
 
 export default function App() {
+  useEffect(() => {
+    Log(
+      "frontend",
+      "info",
+      "component",
+      "Notification application started"
+    );
+  }, []);
+
   return <NotificationsPage />;
 }
